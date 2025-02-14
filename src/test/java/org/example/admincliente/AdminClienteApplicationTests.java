7279758f47cc33b9625ea23816439983cf799f7c
= <artaxerxesnazare@hotmail.com>
2025-02-14 13:09:23 +0100

    strat projetc

---
+++ src/test/java/org/example/admincliente/AdminClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.admincliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
